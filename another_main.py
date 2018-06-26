d45064d3a5acdc0ca74f27a7851c9293e814ec7f
Hou Yuzhou <wtfxljy@gmail.com>
2018-06-26 15:21:05 +0800

    Update another_main.py

--- another_main.py
+++ another_main.py
@@ -9,18 +9,24 @@ def run():
     right_image_path = 'test_r.png'
     left = cv2.imread(left_image_path, cv2.IMREAD_GRAYSCALE)
     right = cv2.imread(right_image_path, cv2.IMREAD_GRAYSCALE)
+    left = cv2.resize(left, (32, 24))
+    right = cv2.resize(right, (32, 24))
     shape = left.shape
-    bm = BM(shape[0], shape[1], diffMethod='NCC')
+
+    bm = BM(shape[0], shape[1], WindowSize=5, numberOfDisparities=4, diffMethod='SAD')
     disp = bm.cvFindCorrespondenceBM()
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     d = sess.run(disp, feed_dict={bm.left_image_raw:np.reshape(left, (bm.image_height,
                                                                       bm.image_width, 1)),
-                                  bm.right_image_raw:np.reshape(right,(bm.image_height,
+                                  bm.right_image_raw:np.reshape(right, (bm.image_height,
                                                                        bm.image_width, 1))})
+    d = np.reshape(d, (24, 32))
     cv2.imshow('disparity', d)
     cv2.imshow('left_image', left)
     cv2.imshow('right_image', right)
+    print(d)
+    cv2.waitKey(0)
 
 
 def main():
